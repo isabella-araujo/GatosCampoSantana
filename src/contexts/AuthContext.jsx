9c9f945eb146231dec0a689fe0660eec594ee92f
Isabella <isabellaoliveira8283@gmail.com>
2025-09-27 13:26:39 -0300

    feat: criar funções de autenticação

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,91 @@
+import { createContext, useContext, useState } from "react";
+import { resetPasswordUser, signInUser, signOutUser, signUpUser } from '../services/authService'
+
+const AuthContext = createContext()
+
+export function AuthProvider({ children }) {
+    const [user, setUser] = useState(null)
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    async function signIn(userData) {
+        setLoading(true)
+
+        const response = await signInUser(userData)
+
+        if(response.user) {
+            setUser(response.user)
+        } else {
+            setError(response.error)
+        }
+
+        setLoading(false)
+    }
+
+    async function signOut() {
+        setLoading(true)
+
+        const response = signOutUser()
+
+        if(response.error) {
+            setError(response.error)
+        } else {
+            setUser(null)
+        }
+
+        setLoading(false)
+    }
+
+    async function signUp(userData) {
+        setLoading(true)
+
+        const response = await signUpUser(userData)
+
+        if(response.user) {
+            setUser(response.user)
+        } else {
+            setError(response.error)
+        }
+
+        setLoading(false)
+    }
+
+    async function resetPassword(userData) {
+        setLoading(true)
+
+        const response = resetPasswordUser(userData)
+
+        if(response.error) {
+            setError(response.error)
+        }
+
+        setLoading(false)
+    }
+
+    const value = {
+        signed: !!user,
+        user,
+        error,
+        loading,
+        signIn,
+        signOut,
+        signUp,
+        resetPassword
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export function useAuth() {
+    const context = useContext(AuthContext)
+
+    if(!context) {
+        throw new Error('useAuth deve ser usado dentro de um AuthProvider')
+    }
+
+    return context
+}
