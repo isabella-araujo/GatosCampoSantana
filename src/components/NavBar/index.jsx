1671c9259f20064810b6ed4c6aa175b49c850d38
Leila <leiila.hort@gmail.com>
2025-10-13 14:55:30 -0300

    feat: criar componente navbar

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import './styles.css';
+import Logo from '../../assets/Logo.svg';
+import { Link } from 'react-router-dom';
+import Button from '../Button';
+import IconButton from '../IconButton';
+import { IoReorderThreeOutline } from 'react-icons/io5';
+import { IoClose } from 'react-icons/io5';
+
+const NavBar = ({ links, navButton }) => {
+  const [menuMobile, setMenuMobile] = useState(false);
+  const menuToggle = () => setMenuMobile(!menuMobile);
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/">
+          <img src={Logo} alt="Logo" />
+        </Link>
+      </div>
+
+      <ul className={`navbar-links ${menuMobile ? 'active' : ''}`}>
+        {links.map((link) => (
+          <li key={link.to}>
+            <Link to={link.to}>{link.label}</Link>
+          </li>
+        ))}
+      </ul>
+      {navButton && (
+        <Link to={navButton.to} className="navbar-btn">
+          <Button variant="secondary" size="small">
+            {navButton.label}
+          </Button>
+        </Link>
+      )}
+      <div className="navbar-menuHamburger">
+        {menuMobile ? (
+          <IconButton
+            onClick={menuToggle}
+            icon={IoClose}
+            color="var( --color-primary-blue)"
+            size={42}
+          />
+        ) : (
+          <IconButton
+            onClick={menuToggle}
+            icon={IoReorderThreeOutline}
+            color="var( --color-primary-blue)"
+            size={48}
+          />
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
